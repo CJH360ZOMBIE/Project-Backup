53a5d66feec06998cf9538d5820a33cf3d289538
CJH360ZOMBIE <charliejhargreaves@gmail.com>
2020-12-05 12:50:30 +0000

    First Commit

---
+++ Project - Copy/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+
+    public float Offset;
+    public Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + Offset); 
+    }
+}
