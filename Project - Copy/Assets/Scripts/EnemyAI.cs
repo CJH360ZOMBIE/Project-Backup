630cce9424b85b8d076eb1676075965678ffaf00
CJH360ZOMBIE <charliejhargreaves@gmail.com>
2020-12-13 16:34:24 +0000

    3

---
+++ Project - Copy/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public Transform EnemyGFX;
+    public float speed = 200f; 
+    public float nextWaypointdistance = 3f;
+
+    Path path; 
+    int currentwaypoint = 0; 
+    bool Reachedendofpath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f );
+
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+         seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentwaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+        return;
+
+        if (currentwaypoint >= path.vectorPath.Count)
+        {
+            Reachedendofpath = true; 
+            return;
+        } else
+        {
+            Reachedendofpath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentwaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentwaypoint]);
+
+        if(distance < nextWaypointdistance)
+        {
+            currentwaypoint++;
+        }
+        //switch enemy rotation depending where the enemy is facing 
+        if (force.x >= 0.01f)
+        {
+          EnemyGFX.localScale = new Vector3 (-1f, 1f, 1f);
+        
+        } else if (force.x <= -0.01f )
+        {
+            EnemyGFX.localScale = new Vector3 (1f, 1f, 1f);
+        }
+    }
+}
---
+++ Project - Copy/Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float laserlength;
+    private LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 endPosition = transform.position + (transform.right * laserlength);
+        lineRenderer.SetPositions(new Vector3[] { transform.position, endPosition });
+    }
+}
